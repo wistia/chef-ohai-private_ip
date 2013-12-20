046188bc4c91e15cad24c98068866e51c8f6981f
Robby Grossman <robby@freerobby.com>
2013-12-20 11:52:39 -0500

    Get private IP based on ifconfig; remove ohai dependency on network/interfaces

--- files/default/plugins/ohai-private_ip.rb
+++ files/default/plugins/ohai-private_ip.rb
@@ -1,7 +1,6 @@
 provides 'private_ip'
 
-require_plugin 'network'
-
-addresses = network.interfaces.map { |name, i| i['addresses'].keys }.flatten
+addresses = `ifconfig | grep inet | grep -v inet6 | awk '{ print $2 }' | cut -f2 -d ':'`.split("\n")
 
 private_ip addresses.grep(/^10\.|^172\.1[6-9]\.|^172\.2\d\.|^172\.3[0-1]\.|^192\.168/).first
+
